f1302a11c5e2577c33c23ff6ea0c136e3800cf9f
Daniel <daniel@iMac-de-Daniel.local>
2021-12-17 13:51:53 -0300

    feat: add global css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import React, { Fragment } from "react";
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <Fragment>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </Fragment>
   );
 }
