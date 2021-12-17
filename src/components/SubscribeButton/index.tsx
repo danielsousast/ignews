6620195eeeba6b37a803618eca79c4062f65bb80
Daniel <daniel@iMac-de-Daniel.local>
2021-12-17 17:24:58 -0300

    feat: get stripe product price

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+interface SubscribeButton {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButton) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import React, { Fragment } from "react";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
 import styles from "./home.module.scss";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <Fragment>
       <Head>
@@ -18,11 +28,31 @@ export default function Home() {
 
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product?.amount} month</span>
           </p>
+
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </Fragment>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K7mQLGSoz3pEZwNYOHMJcJc");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
