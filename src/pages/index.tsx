fbbaf0b9b3981c94094547feddbccc1ae8b3e487
Daniel <daniel@iMac-de-Daniel.local>
2021-12-17 17:36:26 -0300

    feat: add next static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import styles from "./home.module.scss";
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1K7mQLGSoz3pEZwNYOHMJcJc");
 
   const product = {
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 Horas
   };
 };
