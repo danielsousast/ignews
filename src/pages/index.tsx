47e53a1a7ea0ee84f0090edde1fe6126424ca9ab
Daniel <daniel@iMac-de-Daniel.local>
2021-12-17 13:41:05 -0300

    feat: add document next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Head from "next/head";
 
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <Fragment>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </Fragment>
+  );
 }
