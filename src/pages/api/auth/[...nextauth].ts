03fa54cd58b48dca5dfd3462f464ec7cdfcfa6eb
Daniel <daniel@iMac-de-Daniel.local>
2021-12-24 10:47:31 -0300

    feat:add user on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -13,4 +16,24 @@ export default NextAuth({
       },
     }),
   ],
+  jwt: {
+    secret: process.env.APP_SECRET,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: { email },
+          })
+        );
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
